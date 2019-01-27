468d4753ab46e1f28cb01f02f7b1844f60c950f2
johaljicar@hotmail.com <johaljicar@hotmail.com>
2019-01-26 19:17:18 -0500

    se crea el esquema del proyecto en arquitectura onion

---
+++ Insurance-Policy.Services/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance_Policy.Services
+{
+    public class Class1
+    {
+    }
+}
